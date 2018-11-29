4dd04e03a609328177cc5af7fb74d92fa357e86b
Dare Adewuyi <drdre4life2001@yahoo.com>
2018-11-29 23:53:56 +0100

    orders cleanup

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -22,12 +22,9 @@ class OrderController extends BaseController
      */
     public function index()
     {
-        $products = Product::select('name','price')->get();
-
-
-        return $this->sendResponse($products->toArray(), 'Products retrieved successfully.');
+        $orders = Orders::select('name','price')->get();
+        return $this->sendResponse($products->toArray(), 'Orders retrieved successfully.');
     }
-
 
     /**
      * Store a newly created resource in storage.
@@ -69,72 +66,5 @@ class OrderController extends BaseController
         return $this->sendResponse($ordered_products, 'Order was created successfully.');
     }
 
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        
-        $ordered_query = "SELECT products.name, products.price, orderitems.product_id, orderitems.order_id FROM products,orderitems
-        WHERE orderitems.product_id = products.id AND orderitems.order_id = 18";
-        $ordered_products = DB::select($ordered_query);
-        $orders_query;
-
-        if (is_null($product)) {
-            return $this->sendError('Product not found.');
-        }
-
-
-        return $this->sendResponse($product->toArray(), 'Product retrieved successfully.');
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Product $product)
-    {
-        $input = $request->all();
-
-
-        $validator = Validator::make($input, [
-            'name' => 'required',
-            'detail' => 'required'
-        ]);
-
-
-        if ($validator->fails()) {
-            return $this->sendError('Validation Error.', $validator->errors());
-        }
-
-
-        $product->name = $input['name'];
-        $product->detail = $input['detail'];
-        $product->save();
-
-
-        return $this->sendResponse($product->toArray(), 'Product updated successfully.');
-    }
-
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Product $product)
-    {
-        $product->delete();
-
-
-        return $this->sendResponse($product->toArray(), 'Product deleted successfully.');
-    }
+   
 }
