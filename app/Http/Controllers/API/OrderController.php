d0a8a219122415173584ee39a3b49409ddc26ce2
Dare Adewuyi <drdre4life2001@yahoo.com>
2018-12-03 00:53:38 +0100

    order-update

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -22,12 +22,12 @@ class OrderController extends BaseController
      */
     public function index()
     {
-        $orders = Orders::select('name','price')->get();
+        $orders = Orders::select('name', 'price')->get();
         return $this->sendResponse($products->toArray(), 'Orders retrieved successfully.');
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Store a newly created orders in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
@@ -36,35 +36,40 @@ class OrderController extends BaseController
     {
 
         $items = $request->all();
+
         $validator = Validator::make($items, [
             'product_id' => 'required'
         ]);
 
-        $customer_id = Auth::user()->id;
-
         if ($validator->fails()) {
             return $this->sendError('Validation Error.', $validator->errors());
         }
+
+        $customer_id = Auth::user()->id;
         $data = array(
-            'customer_id' => $customer_id,
-            'order_status' => 'ordered'
+            'customer_id' => $customer_id
         );
 
         $order = Order::create($data);
         $order_id = $order->id;
-        $order_data = array(
-            'order_id' => $order_id
-        );
+        $product_ids = explode(",", $items['product_id']);
+        foreach ($product_ids as $id) {
+            $product_id = $items['product_id'];
+            $orderitem = new Orderitem();
+            $orderitem->order_id = $order_id;
+            $orderitem->product_id = $id;
+            $orderitem->save();
+        }
+        $ordered_products = DB::select("SELECT  products.name, products.price,  orderitems.order_id FROM products,orderitems
+        WHERE orderitems.product_id = products.id AND orderitems.order_id = $order_id");
 
-        $product_id = $items['product_id'];
-        $orderitem = new Orderitem();
-        $orderitem->order_id = $order_id;
-        $orderitem->product_id = $items['product_id'];
-        $orderitem->save();
-        $ordered_products = DB::select("SELECT name, sum(price) as total FROM products WHERE id = '$product_id'");
-        
-        return $this->sendResponse($ordered_products, 'Order was created successfully.');
+        $total = DB::select("SELECT SUM(price) AS Total FROM products,orderitems
+         WHERE orderitems.product_id = products.id AND orderitems.order_id = $order_id");
+
+        $order_details = array_merge($ordered_products, $total);
+
+        return $this->sendResponse($order_details, 'Order was created successfully.');
     }
 
-   
+
 }
